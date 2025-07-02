722416d780b1e93ff5ce1e007f75c6469f533492
myota <pppgg2755@gmail.com>
2025-07-02 20:09:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ from itertools import cycle
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 #botstatus
-status = cycle(["KIRA KIRA" ,'ต้องได้เจอกันแน่ๆ "กันดั้มบอกแบบนั้น"'])
+status = cycle([
+    discord.Activity(type=discord.ActivityType.custom, name="KIRA KIRA"),
+    discord.Activity(type=discord.ActivityType.custom, name="💤 I'm sleepy"),
+    discord.Activity(type=discord.ActivityType.custom, name='ต้องได้เจอกันแน่ๆ "กันดั้มบอกแบบนั้น"'),
+])
 @tasks.loop(seconds=5)
 async def botstatus():
-    await bot.change_presence(activity=discord.Game(next(status)))
+    await bot.change_presence(activity=next(status))
 
 
 @bot.event
