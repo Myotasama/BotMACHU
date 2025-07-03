7979daddc79b0b176f7c196b80e4347a34017346
myota <pppgg2755@gmail.com>
2025-07-03 08:03:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -124,8 +124,8 @@ async def helpcommand(interaction):
     embed1 = discord.Embed(title = "Bot Commands",    description="",
                            color = 0x886bbf,
                            timestamp=discord.utils.utcnow())
-    embed1.add_field(name='/clear',value='clear message', inline=False)
-    embed1.add_field(name='/vcstats',value='check vc statics')
+    embed1.add_field(name='/clear',value='clear message')
+    embed1.add_field(name='/vcstats',value='check vc statics',inline=False)
     embed1.add_field(name='/vcranking',value='vc leaderboard', inline=False)
     await  interaction.response.send_message(embed = embed1)
 
